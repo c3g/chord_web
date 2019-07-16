f3fc5c396186f21c7ceda9bfd57f027162a74d5c
David Lougheed <david.lougheed@gmail.com>
2019-07-16 15:37:52 -0400

    Some work on turning dataset searching into a form

--- src/components/discovery/DiscoverySearchContent.js
+++ src/components/discovery/DiscoverySearchContent.js
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 
-import {Typography} from "antd";
+import {Button, Form, Input, Select, Typography} from "antd";
+import "antd/es/button/style/css";
+import "antd/es/form/style/css";
+import "antd/es/input/style/css";
+import "antd/es/select/style/css";
 import "antd/es/typography/style/css";
 
 import SchemaTreeSelect from "../SchemaTreeSelect";
@@ -10,7 +14,37 @@ class DiscoverySchemaContent extends Component {
         return this.props.dataset ? (
             <div>
                 <Typography.Title level={2}>Search Dataset {this.props.dataset.id}</Typography.Title>
-                <SchemaTreeSelect style={{width: 256}} schema={this.props.dataset.schema} />
+
+                <Form>
+                    <Form.Item labelCol={{
+                        lg: {span: 24},
+                        xl: {span: 4}
+                    }} wrapperCol={{
+                        lg: {span: 24},
+                        xl: {span: 20}
+                    }} label="Search Condition">
+                        <Input.Group compact>
+                            <SchemaTreeSelect style={{
+                                width: "288px",
+                                float: "left",
+                                borderTopRightRadius: "0",
+                                borderBottomRightRadius: "0"
+                            }} schema={this.props.dataset.schema} />
+                            <Select style={{width: "128px", float: "left"}}>
+                                <Select.Option key="equals">equals</Select.Option>
+                            </Select>
+                            <Input style={{width: "calc(100% - 416px)"}} placeholder="value" />
+                        </Input.Group>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{
+                        lg: {span: 24},
+                        xl: {offset: 4, span: 20}
+                    }}>
+                        <Button type="primary" htmlType="submit">
+                            Search
+                        </Button>
+                    </Form.Item>
+                </Form>
             </div>
         ) : (<div>Loading...</div>);
     }
