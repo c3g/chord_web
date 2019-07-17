74f349a3fb49ab30845bbda5fac04467f21e940b
David Lougheed <david.lougheed@gmail.com>
2019-07-17 11:04:50 -0400

    Finish fully input-izing schema tree select component

--- src/components/SchemaTreeSelect.js
+++ src/components/SchemaTreeSelect.js
@@ -12,15 +12,28 @@ const generateSchemaTreeSelect = (schema, style, onChange) => (
 );
 
 class SchemaTreeSelect extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ""};
+    static getDerivedStateFromProps(nextProps) {
+        if ("value" in nextProps) {
+            return {...(nextProps.value ? nextProps.value : {})};
+        }
+        return null;
     }
 
-    onChange(value) {
-        this.setState({value});
+    constructor(props) {
+        super(props);
+        const value = props.value ? props.value : {};
+        this.state = {selected: value.selected ? value.selected : undefined};
+    }
+
+    onChange(selected) {
+        // Set the state directly unless value is bound
+        if (!("value" in this.props)) {
+            this.setState({selected});
+        }
+
+        // Update the change handler bound to the component
         if (this.props.onChange) {
-            this.props.onChange(Object.assign({}, this.state, {value}));
+            this.props.onChange(Object.assign({}, this.state, {selected}));
         }
     }
 
