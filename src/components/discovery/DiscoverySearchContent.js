0ebd9625fa4b9b833f971250c6ae0842cc92a41a
David Lougheed <david.lougheed@gmail.com>
2019-08-15 15:15:22 -0400

    Move constants out of render

--- src/components/discovery/DiscoverySearchContent.js
+++ src/components/discovery/DiscoverySearchContent.js
@@ -17,6 +17,72 @@ import "antd/es/typography/style/css";
 import DiscoverySearchForm from "./DiscoverySearchForm";
 import {performSearch, selectSearch, updateDiscoverySearchForm} from "../../actions";
 
+const DATA_USE_KEYS = ["COL", "IRB", "GS", "IS", "NPU", "PS", "MOR", "PUB", "RTN", "TS", "US"];
+
+const DATA_USE_INFO = {
+    COL: {
+        icon: "team",
+        title: "Collaboration Required",
+        content: "This requirement indicates that the requester must agree to collaboration with the primary " +
+            "study investigator(s)."
+    },
+    IRB: {
+        icon: "reconciliation",
+        title: "Ethics Approval Required",
+        content: "This requirement indicates that the requester must provide documentation of local IRB/ERB" +
+            "approval."
+    },
+    GS: {
+        icon: "global",
+        title: "Geographical Restriction",
+        content: "This requirement indicates that use is limited to within a specific geographic region."
+    },
+    IS: {
+        icon: "bank",
+        title: "Institution-Specific Restriction",
+        content: "This requirement indicates that use is limited to use within an approved institution."
+    },
+    NPU: {
+        icon: "dollar", // Gets modified below
+        title: "Not-For-Profit Use Only",
+        content: "This requirement indicates that use of the data is limited to not-for-profit organizations " +
+            "and not-for-profit use, non-commercial use."
+    },
+    PS: {
+        icon: "audit",
+        title: "Project-Specific Restriction",
+        content: "This requirement indicates that use is limited to use within an approved project."
+    },
+    MOR: {
+        icon: "exception",
+        title: "Publication Moratorium",
+        content: "This requirement indicates that requester agrees not to publish results of studies until a " +
+            "specific date"
+    },
+    PUB: {
+        icon: "file-done",
+        title: "Publication Required",
+        content: "This requirement indicates that requester agrees to make results of studies using the data " +
+            "available to the larger scientific community."
+    },
+    RTN: {
+        icon: "database",
+        title: "Return to Database or Resource",
+        content: "This requirement indicates that the requester must return derived/enriched data to the " +
+            "database/resource."
+    },
+    TS: {
+        icon: "clock-circle",
+        title: "Time Limit on Use",
+        content: "This requirement indicates that use is approved for a specific number of months."
+    },
+    US: {
+        icon: "user",
+        title: "User-Specific Restriction",
+        content: "This requirement indicates that use is limited to use by approved users."
+    }
+};
+
 class DiscoverySearchContent extends Component {
     constructor(props) {
         super(props);
@@ -46,72 +112,6 @@ class DiscoverySearchContent extends Component {
         if (!this.props.dataType || !this.props.searches || this.props.searches.length === 0) return (
             <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No Searches" />
         );
-
-        const DATA_USE_KEYS = ["COL", "IRB", "GS", "IS", "NPU", "PS", "MOR", "PUB", "RTN", "TS", "US"];
-
-        const DATA_USE_INFO = {
-            COL: {
-                icon: "team",
-                title: "Collaboration Required",
-                content: "This requirement indicates that the requester must agree to collaboration with the primary " +
-                    "study investigator(s)."
-            },
-            IRB: {
-                icon: "reconciliation",
-                title: "Ethics Approval Required",
-                content: "This requirement indicates that the requester must provide documentation of local IRB/ERB" +
-                    "approval."
-            },
-            GS: {
-                icon: "global",
-                title: "Geographical Restriction",
-                content: "This requirement indicates that use is limited to within a specific geographic region."
-            },
-            IS: {
-                icon: "bank",
-                title: "Institution-Specific Restriction",
-                content: "This requirement indicates that use is limited to use within an approved institution."
-            },
-            NPU: {
-                icon: "dollar", // Gets modified below
-                title: "Not-For-Profit Use Only",
-                content: "This requirement indicates that use of the data is limited to not-for-profit organizations " +
-                    "and not-for-profit use, non-commercial use."
-            },
-            PS: {
-                icon: "audit",
-                title: "Project-Specific Restriction",
-                content: "This requirement indicates that use is limited to use within an approved project."
-            },
-            MOR: {
-                icon: "exception",
-                title: "Publication Moratorium",
-                content: "This requirement indicates that requester agrees not to publish results of studies until a " +
-                    "specific date"
-            },
-            PUB: {
-                icon: "file-done",
-                title: "Publication Required",
-                content: "This requirement indicates that requester agrees to make results of studies using the data " +
-                    "available to the larger scientific community."
-            },
-            RTN: {
-                icon: "database",
-                title: "Return to Database or Resource",
-                content: "This requirement indicates that the requester must return derived/enriched data to the " +
-                    "database/resource."
-            },
-            TS: {
-                icon: "clock-circle",
-                title: "Time Limit on Use",
-                content: "This requirement indicates that use is approved for a specific number of months."
-            },
-            US: {
-                icon: "user",
-                title: "User-Specific Restriction",
-                content: "This requirement indicates that use is limited to use by approved users."
-            }
-        };
 
         return (
             <Collapse bordered={false} accordion={true} activeKey={this.props.selectedSearch.toString()}
