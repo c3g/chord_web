a4e42426a17aa9873bccd656f8bd2882cf4fd41a
David Lougheed <david.lougheed@gmail.com>
2019-09-09 11:16:23 -0400

    Add proper workflow loading and enabling/disabling button based on wf inputs

--- src/components/manager/ManagerFilesIngestionContent.js
+++ src/components/manager/ManagerFilesIngestionContent.js
@@ -11,29 +11,71 @@ import "antd/es/tree/style/css";
 
 import {LAYOUT_CONTENT_STYLE} from "../../styles/layoutContent";
 
-const menu = (
-    <Menu>
-        <Menu.Item key="1">
-            Ingest with Workflow 1
-        </Menu.Item>
-    </Menu>
-);
-
 class ManagerFilesIngestionContent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedFile: null
+        };
+
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    handleSelect(keys) {
+        if (keys.includes("root")) {
+            this.setState({selectedFile: null});
+            return;
+        }
+
+        this.setState({selectedFile: keys[0] || null});
+    }
+
     render() {
+        // TODO: Loading for workflows...
+        // TODO: Proper workflow keys
+
+        let oneWorkflowSupported = false;
+
+        const workflowMenu = (
+            <Menu>
+                {this.props.workflows.map(w => {
+                    // TODO: Extend to multiple files by checking all file inputs are matched
+
+                    const workflowSupported = w.inputs
+                        .filter(i => i.type === "file")
+                        .filter(i => i.extensions
+                            .filter(e => this.state.selectedFile && this.state.selectedFile.endsWith(e)).length > 0)
+                        .length > 0;
+
+                    oneWorkflowSupported = oneWorkflowSupported || workflowSupported;
+
+                    return (
+                        <Menu.Item key={w.name} disabled={!workflowSupported}>
+                            Ingest with Workflow "{w.name}"
+                        </Menu.Item>
+                    );
+                })}
+            </Menu>
+        );
+
         // TODO: support directories as well
+        // TODO: Loading for files...
         const files = this.props.files.map(f => <Tree.TreeNode title={f} key={f} isLeaf={true} />);
+
         return (
             <Layout>
                 <Layout.Content style={LAYOUT_CONTENT_STYLE}>
                     <div style={{marginBottom: "1em"}}>
-                        <Dropdown.Button overlay={menu} style={{marginRight: "10px"}}>
+                        <Dropdown.Button overlay={workflowMenu} style={{marginRight: "10px"}}
+                                         disabled={!this.state.selectedFile || !oneWorkflowSupported}>
                             <Icon type="import" /> Ingest
                         </Dropdown.Button>
-                        <Button type="danger" icon="delete">Delete</Button>
+                        <Button type="danger" icon="delete" disabled={!this.state.selectedFile}>Delete</Button>
                         <Button type="primary" icon="upload" style={{float: "right"}}>Upload</Button>
                     </div>
-                    <Tree.DirectoryTree defaultExpandAll={true}>
+                    <Tree.DirectoryTree defaultExpandAll={true} onSelect={this.handleSelect}
+                                        selectedKeys={this.state.selectedFile ? [this.state.selectedFile] : []}>
                         <Tree.TreeNode title="chord_drop_box" key="root">{files}</Tree.TreeNode>
                     </Tree.DirectoryTree>
                 </Layout.Content>
@@ -43,7 +85,10 @@ class ManagerFilesIngestionContent extends Component {
 }
 
 const mapStateToProps = state => ({
-    files: state.dropBox.tree
+    files: state.dropBox.tree,
+    workflows: Object.values(state.serviceWorkflows.workflowsByServiceID)
+        .filter(s => !s.isFetching)
+        .flatMap(s => Object.values(s.workflows.ingestion))
 });
 
 export default connect(mapStateToProps)(ManagerFilesIngestionContent);
