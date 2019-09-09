339ad226d97cc178159f6578142a2c25efff990f
David Lougheed <david.lougheed@gmail.com>
2019-09-09 15:39:32 -0400

    Render different content at different steps

--- src/components/manager/ManagerIngestionContent.js
+++ src/components/manager/ManagerIngestionContent.js
@@ -16,27 +16,73 @@ import {workflowsStateToPropsMixin, workflowsStateToPropsMixinPropTypes} from ".
 import {LAYOUT_CONTENT_STYLE} from "../../styles/layoutContent";
 
 class ManagerIngestionContent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            step: 0,
+            selectedWorkflow: null
+        };
+
+        this.handleStepChange = this.handleStepChange.bind(this);
+    }
+
+    handleClick(workflow) {
+        this.setState({
+            selectedWorkflow: workflow.name,
+            step: 1
+        });
+    }
+
+    handleStepChange(step) {
+        this.setState({step});
+    }
+
     render() {
-        const workflows = this.props.workflows.map(w => (
-            <List.Item key={w.name}><WorkflowListItem key={w.name} workflow={w} selectable={true} /></List.Item>
-        ));  // TODO: real key;
+        let stepContents = null;
+        switch (this.state.step) {
+            case 0:
+                const workflows = this.props.workflows.map(w => (
+                    <List.Item key={w.name}>
+                        <WorkflowListItem key={w.name} workflow={w} selectable={true}
+                                          onClick={() => this.handleClick(w)} />
+                    </List.Item>
+                ));  // TODO: real key
+
+                stepContents = (
+                    <Spin spinning={this.props.workflowsLoading}>
+                        {this.props.workflowsLoading
+                            ? <Skeleton />
+                            : <List itemLayout="vertical">{workflows}</List>}
+                    </Spin>
+                );
+
+                break;
+
+            case 1:
+                stepContents = (<div>step 2</div>);
+                break;
+
+            case 2:
+                stepContents = (<div>step 3</div>);
+                break;
+        }
+
         return (
             <Layout>
                 <Layout.Content style={LAYOUT_CONTENT_STYLE}>
-                    <Steps current={0}>
-                        <Steps.Step title="Ingestion Workflow"
-                                    description={`Choose a relevant ingestion workflow.`} />
+                    <Steps current={this.state.step} onChange={this.handleStepChange}>
+                        <Steps.Step title="Workflow"
+                                    description={`Choose a relevant ingestion workflow.`}>
+
+                        </Steps.Step>
                         <Steps.Step title="Input"
-                                    description="Select input data for the workflow." />
-                        <Steps.Step title="Run" description="Confirm details and run the workflow." />
+                                    description="Select input data for the workflow."
+                                    disabled={this.state.step < 1} />
+                        <Steps.Step title="Run" description="Confirm details and run the workflow."
+                                    disabled={this.state.step < 2} />
                     </Steps>
-                    <div style={{marginTop: "16px"}}>
-                        <Spin spinning={this.props.workflowsLoading}>
-                            {this.props.workflowsLoading
-                                ? <Skeleton />
-                                : <List itemLayout="vertical">{workflows}</List>}
-                        </Spin>
-                    </div>
+                    <div style={{marginTop: "16px"}}>{stepContents}</div>
                 </Layout.Content>
             </Layout>
         )
