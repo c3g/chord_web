615c0009288c02f1001043e02e61f6f8c34c223e
David Lougheed <david.lougheed@gmail.com>
2019-09-20 15:55:15 -0400

    Condense ingestion input form and allow tree select to traverse directories

--- src/components/manager/IngestionInputForm.js
+++ src/components/manager/IngestionInputForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-import {Button, Form, Icon, Input, Select, TreeSelect} from "antd";
+import {Button, Form, Icon, Input, Select, Tree, TreeSelect} from "antd";
 
 import "antd/es/button/style/css";
 import "antd/es/form/style/css";
@@ -9,12 +9,20 @@ import "antd/es/input/style/css";
 import "antd/es/select/style/css";
 import "antd/es/tree-select/style/css";
 
+const generateFileTree = (directory, valid) => directory.map(entry =>
+    <TreeSelect.TreeNode title={entry.name} key={entry.path} value={entry.path} disabled={!valid(entry)}
+                         isLeaf={!entry.hasOwnProperty("contents")}
+                         selectable={!entry.hasOwnProperty("contents")}>
+        {(entry || {contents: []}).contents ? generateFileTree(entry.contents, valid) : null}
+    </TreeSelect.TreeNode>);
+
 class IngestionInputForm extends Component {
     constructor(props) {
         super(props);
         this.onSubmit = this.props.onSubmit || (() => {});
         this.onBack = this.props.onBack || null;
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.getInputComponent = this.getInputComponent.bind(this);
     }
 
     handleSubmit(e) {
@@ -25,63 +33,66 @@ class IngestionInputForm extends Component {
         })
     }
 
+    getInputComponent(input) {
+        let inputComponent = <Input />;
+
+        switch (input.type) {
+            case "file":
+                inputComponent = (
+                    <TreeSelect showSearch={true} treeDefaultExpandAll={true}>
+                        <TreeSelect.TreeNode title="chord_drop_box" key="root">
+                            {generateFileTree(
+                                this.props.tree,
+                                entry => entry.hasOwnProperty("contents") ||
+                                    input.extensions.find(e => entry.name.endsWith(e)) !== undefined
+                            )}
+                        </TreeSelect.TreeNode>
+                    </TreeSelect>
+                );
+                break;
+
+            case "enum":
+                inputComponent = (
+                    <Select>
+                        {input.values.map(v => <Select.Option key={v} value={v}>{v}</Select.Option>)}
+                    </Select>
+                );
+                break;
+
+            case "number":
+                inputComponent = <Input type="number"/>;
+                break;
+        }
+
+        return inputComponent;
+    }
+
     render() {
-        let formContents = this.props.workflow.inputs.map(i => {
-            let inputComponent = <Input />;
-
-            switch (i.type) {
-                case "file":
-                    const files = this.props.tree
-                        .filter(f => i.extensions.find(e => f.name.endsWith(e)))
-                        .map(f => <TreeSelect.TreeNode title={f.name} key={f.path} value={f.path} isLeaf={true} />);
-                    inputComponent = (
-                        <TreeSelect showSearch={true} treeDefaultExpandAll={true}>
-                            <TreeSelect.TreeNode title="chord_drop_box" key="root">{files}</TreeSelect.TreeNode>
-                        </TreeSelect>
-                    );
-                    break;
-
-                case "enum":
-                    inputComponent = (
-                        <Select>
-                            {i.values.map(v => <Select.Option key={v} value={v}>{v}</Select.Option>)}
-                        </Select>
-                    );
-                    break;
-
-                case "number":
-                    inputComponent = <Input type="number"/>;
-                    break;
-            }
-
-            return (
-                <Form.Item label={i.id} key={i.id}>
-                    {this.props.form.getFieldDecorator(i.id, {
-                        rules: [{required: true}]
-                    })(inputComponent)}
-                </Form.Item>
-            );
-        });
-
-        formContents.push(
-            <Form.Item key="_submit" wrapperCol={{
-                md: {span: 24},
-                lg: {offset: 4, span: 16},
-                xl: {offset: 6, span: 12},
-                xxl: {offset: 8, span: 8}
-            }}>
-                {this.onBack ? <Button icon="left" onClick={() => this.onBack()}>Back</Button> : null}
-                <Button type="primary" htmlType="submit" style={{float: "right"}}>
-                    Next <Icon type="right" />
-                </Button>
-            </Form.Item>
-        );
-
         return (
             <Form labelCol={{md: {span: 24}, lg: {span: 4}, xl: {span: 6}, xxl: {span: 8}}}
                   wrapperCol={{md: {span: 24}, lg: {span: 16}, xl: {span: 12}, xxl: {span: 8}}}
                   onSubmit={this.handleSubmit}>
-                {formContents}
+                {[
+                    ...this.props.workflow.inputs.map(i => (
+                        <Form.Item label={i.id} key={i.id}>
+                            {this.props.form.getFieldDecorator(i.id, {
+                                rules: [{required: true}]
+                            })(this.getInputComponent(i))}
+                        </Form.Item>
+                    )),
+
+                    <Form.Item key="_submit" wrapperCol={{
+                        md: {span: 24},
+                        lg: {offset: 4, span: 16},
+                        xl: {offset: 6, span: 12},
+                        xxl: {offset: 8, span: 8}
+                    }}>
+                        {this.onBack ? <Button icon="left" onClick={() => this.onBack()}>Back</Button> : null}
+                        <Button type="primary" htmlType="submit" style={{float: "right"}}>
+                            Next <Icon type="right" />
+                        </Button>
+                    </Form.Item>
+                ]}
             </Form>
         );
     }
