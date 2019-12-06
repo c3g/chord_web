a1e0155cc0825e328c5d50d77f6d223f176f7221
David Lougheed <david.lougheed@gmail.com>
2019-12-06 16:28:23 -0500

    Tweak wes event regex

--- src/modules/wes/events.js
+++ src/modules/wes/events.js
@@ -3,7 +3,7 @@ import {receiveRunDetails} from "./actions";
 const EVENT_WES_RUN_UPDATED = "wes_run_updated";
 
 export default {
-    "chord.service.wes": message => async dispatch => {
+    "^chord.service.wes$": message => async dispatch => {
         if (message.type === EVENT_WES_RUN_UPDATED) {
             await dispatch(receiveRunDetails(message.data.run_id, message.data));
         }
