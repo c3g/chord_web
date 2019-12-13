e286be5aa71a1aa19d4e34b7a602e6c45b3942b1
David Lougheed <david.lougheed@gmail.com>
2019-12-13 16:40:19 -0500

    Make ingestion forms a bit wider

--- src/components/manager/ingestion.js
+++ src/components/manager/ingestion.js
@@ -1,8 +1,8 @@
-export const FORM_LABEL_COL = {md: {span: 24}, lg: {span: 4}, xl: {span: 6}, xxl: {span: 8}};
-export const FORM_WRAPPER_COL = {md: {span: 24}, lg: {span: 16}, xl: {span: 12}, xxl: {span: 8}};
+export const FORM_LABEL_COL = {md: {span: 24}, lg: {span: 4}, xl: {span: 6}, xxl: {span: 7}};
+export const FORM_WRAPPER_COL = {md: {span: 24}, lg: {span: 16}, xl: {span: 12}, xxl: {span: 10}};
 export const FORM_BUTTON_COL = {
     md: {span: 24},
     lg: {offset: 4, span: 16},
     xl: {offset: 6, span: 12},
-    xxl: {offset: 8, span: 8}
+    xxl: {offset: 7, span: 10}
 };
