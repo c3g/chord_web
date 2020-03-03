8f24ebeeb533d4d9a59762b7caac6917ea717c58
David Lougheed <david.lougheed@gmail.com>
2020-03-03 16:03:00 -0500

    More work on discovery search form

--- src/components/discovery/DiscoverySearchForm.js
+++ src/components/discovery/DiscoverySearchForm.js
@@ -42,53 +42,35 @@ class DiscoverySearchForm extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {conditionsHelp: {}, fieldSchemas: {}};
-        this.initialValues = {keys: []};
+        this.initialValues = {};
 
-        this.handleFieldChange = this.handleFieldChange.bind(this);
         this.getDataTypeFieldSchema = this.getDataTypeFieldSchema.bind(this);
-        this.addCondition = this.addCondition.bind(this);
-        this.removeCondition = this.removeCondition.bind(this);
+        this.makeCondition = this.makeCondition.bind(this);
 
         this.form = React.createRef();
     }
 
     componentDidMount() {
-        // TODO: MAKE THIS WORK this.addCondition(); // Make sure there's one condition at least
-        if (this.form.current.getFieldValue("keys").length !== 0) return;
-
         const requiredFields = this.props.dataType
             ? getFields(this.props.dataType.schema).filter(f =>
                 (getFieldSchema(this.props.dataType.schema, f).search || {required: false}).required || false)
             : [];
 
-        const stateUpdates = requiredFields.map(c => this.addCondition(c, undefined, true));
+        this.initialValues = {
+            ...this.initialValues,
+            conditions: requiredFields.map(c => this.makeCondition(c, undefined, true))
+        };
 
-        // Add a single default condition if necessary
         if (requiredFields.length === 0 && this.props.conditionType !== "join") {
-            stateUpdates.push(this.addCondition(undefined, undefined, true));
+            this.initialValues = {
+                ...this.initialValues,
+                conditions: [this.makeCondition(undefined, undefined, true)]
+            };
         }
 
-        this.setState({
-            ...stateUpdates.reduce((acc, v) => ({
-                ...acc, conditionsHelp: {...(acc.conditionsHelp || {}), ...(v.conditionsHelp || {})}
-            }), {})
-        });
-    }
+        this.form.current.setFieldsValue(this.initialValues);
 
-    handleFieldChange(k, change) {
-        this.setState({
-            conditionsHelp: {
-                ...this.state.conditionsHelp,
-                [k]: change.fieldSchema.description || undefined,
-            }
-        })
-    }
-
-    removeCondition(k) {
-        this.form.current.setFieldsValue({
-            keys: this.form.current.getFieldValue("keys").filter(key => key !== k)
-        });
+        // TODO: Add all required fields
     }
 
     getDataTypeFieldSchema(field) {
@@ -102,10 +84,8 @@ class DiscoverySearchForm extends Component {
         };
     }
 
-    addCondition(field = undefined, field2 = undefined, didMount = false) {
+    makeCondition(field = undefined, field2 = undefined, didMount = false) {
         const conditionType = this.props.conditionType || "data-type";
-
-        const newKey = this.form.current.getFieldValue("keys").length;
 
         // TODO: What if operations is an empty list?
 
@@ -113,28 +93,14 @@ class DiscoverySearchForm extends Component {
             ? this.getDataTypeFieldSchema(field)
             : {search: {...DEFAULT_SEARCH_PARAMETERS}};  // Join search conditions have all operators "available" TODO
 
-        const stateUpdate = {
-            conditionsHelp: {
-                ...this.state.conditionsHelp,
-                [newKey]: fieldSchema.description || undefined
-            }
+        return {
+            field,
+            ...(conditionType === "data-type" ? {} : {field2}),
+            fieldSchema,
+            negated: false,
+            operation: ((fieldSchema || {search: {}}).search.operations || [OP_EQUALS])[0] || OP_EQUALS,
+            ...(conditionType === "data-type" ? {searchValue: ""} : {})
         };
-
-        if (!didMount) this.setState(stateUpdate);  // Won't fire properly in componentDidMount
-
-        this.form.current.setFieldsValue({
-            keys: this.form.current.getFieldValue("keys").concat(newKey),
-            [`conditions[${newKey}]`]: {
-                field,
-                ...(conditionType === "data-type" ? {} : {field2}),
-                fieldSchema,
-                negated: false,
-                operation: ((fieldSchema || {search: {}}).search.operations || [OP_EQUALS])[0] || OP_EQUALS,
-                ...(conditionType === "data-type" ? {searchValue: ""} : {})
-            }
-        });
-
-        return stateUpdate;
     }
 
     cannotBeUsed(fieldString) {
@@ -150,51 +116,106 @@ class DiscoverySearchForm extends Component {
     }
 
     render() {
-        const getCondition = ck => this.form.current.getFieldValue(`conditions[${ck}]`);
+        // const getCondition = ck => this.form.current.getFieldValue(`conditions[${ck}]`);
 
-        let formItems = [];
+        // let formItems = [];
+        //
+        // if (this.form.current) {
+        //     const keys = this.form.current.getFieldValue("keys");
+        //     const existingUniqueFields = keys
+        //         .filter(k => k !== undefined)
+        //         .map(k => getCondition(k).field)
+        //         .filter(f => f !== undefined && this.cannotBeUsed(f));
+        //
+        //     formItems = keys.map((k, i) => (
+        //         <Form.Item key={k}
+        //                    labelCol={{lg: {span: 24}, xl: {span: 4}, xxl: {span: 3}}}
+        //                    wrapperCol={{lg: {span: 24}, xl: {span: 20}, xxl: {span: 18}}}
+        //                    label={`Condition ${i + 1}`}
+        //                    help={this.state.conditionsHelp[k] || undefined}
+        //                    name={`conditions[${k}]`}
+        //                    validateTrigger={false}
+        //                    rules={CONDITION_RULES}>
+        //             <DiscoverySearchCondition conditionType={this.props.conditionType || "data-type"}
+        //                                       dataType={this.props.dataType}
+        //                                       isExcluded={f => existingUniqueFields.includes(f) || this.isNotPublic(f)}
+        //                                       onFieldChange={change => this.handleFieldChange(k, change)}
+        //                                       onRemoveClick={() => this.removeCondition(k)}
+        //                                       removeDisabled={(() => {
+        //                                           if (this.props.conditionType === "join") return false;
+        //                                           if (keys.length <= 1) return true;
+        //
+        //                                           const conditionValue = getCondition(k);
+        //
+        //                                           // If no field has been selected, it's removable
+        //                                           if (!conditionValue.field) return false;
+        //
+        //                                           return keys.map(getCondition)
+        //                                               .filter(cv => ((cv.fieldSchema || {}).search || {}).required
+        //                                                   && cv.field === conditionValue.field).length <= 1;
+        //                                       })()}/>
+        //         </Form.Item>
+        //     ));
+        // }
 
-        if (this.form.current) {
-            const keys = this.form.current.getFieldValue("keys");
-            const existingUniqueFields = keys
-                .filter(k => k !== undefined)
-                .map(k => getCondition(k).field)
+        const getFields = (fields, {add, remove}) => {
+            const conditionType = this.props.conditionType || "data-type";
+
+            const existingUniqueFields = fields
+                .map(f => f.value)
                 .filter(f => f !== undefined && this.cannotBeUsed(f));
 
-            formItems = keys.map((k, i) => (
-                <Form.Item key={k}
-                           labelCol={{lg: {span: 24}, xl: {span: 4}, xxl: {span: 3}}}
-                           wrapperCol={{lg: {span: 24}, xl: {span: 20}, xxl: {span: 18}}}
-                           label={`Condition ${i + 1}`}
-                           help={this.state.conditionsHelp[k] || undefined}
-                           name={`conditions[${k}]`}
-                           validateTrigger={false}
-                           rules={CONDITION_RULES}>
-                    <DiscoverySearchCondition conditionType={this.props.conditionType || "data-type"}
-                                              dataType={this.props.dataType}
-                                              isExcluded={f => existingUniqueFields.includes(f) || this.isNotPublic(f)}
-                                              onFieldChange={change => this.handleFieldChange(k, change)}
-                                              onRemoveClick={() => this.removeCondition(k)}
-                                              removeDisabled={(() => {
-                                                  if (this.props.conditionType === "join") return false;
-                                                  if (keys.length <= 1) return true;
+            return (
+                <>
+                    {fields.map((field, i) => {
+                        const fieldSchema = field && conditionType === "data-type"
+                            ? this.getDataTypeFieldSchema(
+                                (this.form.current.getFieldValue("conditions")[i] || {}).field)
+                            : {search: {...DEFAULT_SEARCH_PARAMETERS}};
 
-                                                  const conditionValue = getCondition(k);
+                        return (
+                            <Form.Item {...field}
+                                       labelCol={{lg: {span: 24}, xl: {span: 4}, xxl: {span: 3}}}
+                                       wrapperCol={{lg: {span: 24}, xl: {span: 20}, xxl: {span: 18}}}
+                                       label={`Condition ${i + 1}`}
+                                       help={fieldSchema.description || undefined}
+                                       validateTrigger={false}
+                                       rules={CONDITION_RULES}>
+                                <DiscoverySearchCondition conditionType={conditionType}
+                                                          dataType={this.props.dataType}
+                                                          isExcluded={f => existingUniqueFields.includes(f)
+                                                              || this.isNotPublic(f)}
+                                                          onRemoveClick={() => remove(field.name)}
+                                                          removeDisabled={(() => {
+                                                              if (this.props.conditionType === "join") return false;
+                                                              if (fields.length <= 1) return true;
 
-                                                  // If no field has been selected, it's removable
-                                                  if (!conditionValue.field) return false;
+                                                              // If no field has been selected, it's removable
+                                                              if (!field.value || !field.value.field) return false;
 
-                                                  return keys.map(getCondition)
-                                                      .filter(cv => ((cv.fieldSchema || {}).search || {}).required
-                                                          && cv.field === conditionValue.field).length <= 1;
-                                              })()}/>
-                </Form.Item>
-            ));
-        }
+                                                              return fields.map(f => f.value)
+                                                                  .filter(cv => ((cv.fieldSchema || {}).search
+                                                                      || {}).required && cv.field === field.value.field
+                                                                  ).length <= 1;
+                                                          })()}/>
+                            </Form.Item>
+                        )
+                    })}
+                    <Form.Item wrapperCol={{
+                        xl: {span: 24},
+                        xxl: {offset: 3, span: 18}
+                    }}>
+                        <Button type="dashed" onClick={() => add()} style={{width: "100%"}}>
+                            <PlusOutlined /> Add condition
+                        </Button>
+                    </Form.Item>
+                </>
+            );
+        };
 
         return (
             <Form ref={this.form}
-                  onFieldsChange={({onChange}, _, allFields) => {onChange({...allFields})}}
+                  onFieldsChange={(_, allFields) => this.props.onChange({...allFields})}
                   // mapPropsToFields={({formValues}) => ({
                   //     keys: Form.createFormField({...formValues.keys}),
                   //     ...Object.assign({}, ...(formValues["conditions"] || [])
@@ -203,17 +224,7 @@ class DiscoverySearchForm extends Component {
                   // })}
                   onFinish={this.onFinish}
                   initialValues={this.initialValues}>
-                <Form.List>
-                    {formItems}
-                </Form.List>
-                <Form.Item wrapperCol={{
-                    xl: {span: 24},
-                    xxl: {offset: 3, span: 18}
-                }}>
-                    <Button type="dashed" onClick={() => this.addCondition()} style={{width: "100%"}}>
-                        <PlusOutlined /> Add condition
-                    </Button>
-                </Form.Item>
+                <Form.List name="conditions">{getFields}</Form.List>
             </Form>
         );
     }
