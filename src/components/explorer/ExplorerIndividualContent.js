ca3d86521d64b2ca60732ca3d02da787f1f96d86
David Lougheed <david.lougheed@gmail.com>
2020-05-28 14:23:40 -0400

    Debug print

--- src/components/explorer/ExplorerIndividualContent.js
+++ src/components/explorer/ExplorerIndividualContent.js
@@ -73,6 +73,8 @@ class ExplorerIndividualContent extends Component {
             ? matchingMenuKeys(individualMenu, urlPath(this.props.nodeInfo.CHORD_URL))
             : [];
 
+        console.log(this.state.backUrl);
+
         return <>
             <SitePageHeader title={(individual || {}).id || "Loading..."}
                             withTabBar={true}
