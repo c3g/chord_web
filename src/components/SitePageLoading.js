5188099da680ee6f15943fdc3e53aab85d58bf07
David Lougheed <david.lougheed@gmail.com>
2020-05-29 10:01:30 -0400

    More loading page tweaks

--- src/components/SitePageLoading.js
+++ src/components/SitePageLoading.js
@@ -5,8 +5,8 @@ import {Skeleton} from "antd";
 import "antd/es/skeleton/style/css";
 
 const SitePageLoading = () => <>
-    <SitePageHeader title={<Skeleton active={true} title={{width: 100}} paragraph={false} />} />
-    <div style={{padding: "32px 24px", background: "white"}}>
+    <SitePageHeader title="&nbsp;" />
+    <div style={{padding: "24px", background: "white"}}>
         <Skeleton title={false} active={true} />
     </div>
 </>;
