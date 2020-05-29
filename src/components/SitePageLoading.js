b68d7d017f65a6c47c7689fdd0b14246029aa5ed
David Lougheed <david.lougheed@gmail.com>
2020-05-29 09:52:30 -0400

    Try to improve loading placeholder a bit

--- src/components/SitePageLoading.js
+++ src/components/SitePageLoading.js
@@ -5,9 +5,9 @@ import {Skeleton} from "antd";
 import "antd/es/skeleton/style/css";
 
 const SitePageLoading = () => <>
-    <SitePageHeader title="&nbsp;" />
+    <SitePageHeader title={<Skeleton active={true} title={{width: 100}} paragraph={false} />} />
     <div style={{padding: "32px 24px", background: "white"}}>
-        <Skeleton title={false} />
+        <Skeleton title={false} active={true} />
     </div>
 </>;
 
