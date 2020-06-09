89f7ba5eeb28178b4bb3541bb4bc6da4c456197f
David Lougheed <david.lougheed@gmail.com>
2020-06-09 09:37:33 -0400

    Remove pointless awaits

--- src/modules/services/actions.js
+++ src/modules/services/actions.js
@@ -133,11 +133,11 @@ export const fetchServicesWithMetadataAndDataTypesAndTables = () => async (dispa
     dispatch(endFlow(LOADING_ALL_SERVICE_DATA));
 };
 
-export const fetchServicesWithMetadataAndDataTypesAndTablesIfNeeded = () => async (dispatch, getState) => {
+export const fetchServicesWithMetadataAndDataTypesAndTablesIfNeeded = () => (dispatch, getState) => {
     const state = getState();
     if ((state.chordServices.items.length === 0 || state.services.items.length === 0 ||
             Object.keys(state.serviceDataTypes.dataTypesByServiceID).length === 0) &&
             !state.services.isFetchingAll) {
-        await dispatch(fetchServicesWithMetadataAndDataTypesAndTables());
+        return dispatch(fetchServicesWithMetadataAndDataTypesAndTables());
     }
 };
