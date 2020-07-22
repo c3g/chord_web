0d39c82e2025b3c4cf4ce5c9f83121b5c1cd3b80
David Lougheed <david.lougheed@gmail.com>
2020-07-22 11:34:05 -0400

    Split service detail into overview/logs tabs

--- src/components/services/ServiceDetail.js
+++ src/components/services/ServiceDetail.js
@@ -1,47 +1,69 @@
-import React, {Component} from "react";
+import React, {Component, Suspense} from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 
-import {Layout, Skeleton, Typography} from "antd";
+import {Menu, Skeleton} from "antd";
 import "antd/es/layout/style/css";
 import "antd/es/skeleton/style/css";
-import "antd/es/typography/style/css";
 
 import SitePageHeader from "../SitePageHeader";
+import ServiceOverview from "./ServiceOverview";
+import ServiceLogs from "./ServiceLogs";
 
-import {withBasePath} from "../../utils/url";
-import {LAYOUT_CONTENT_STYLE} from "../../styles/layoutContent";
+import {nodeInfoDataPropTypesShape} from "../../propTypes";
+import {matchingMenuKeys, renderMenuItem} from "../../utils/menu";
+import {urlPath, withBasePath} from "../../utils/url";
+import {Redirect, Route, Switch} from "react-router-dom";
+
+const pageMenu = artifact => [
+    {url: withBasePath(`services/${artifact}/overview`), style: {marginLeft: "4px"}, text: "Overview"},
+    {url: withBasePath(`services/${artifact}/logs`), text: "Logs"},
+];
+
+// TODO: Deduplicate with data manager
+const MENU_STYLE = {
+    marginLeft: "-24px",
+    marginRight: "-24px",
+    marginTop: "-12px"
+};
 
 class ServiceDetail extends Component {
     render() {
         // TODO: 404
-        const serviceInfo = this.props.serviceInfoByArtifact[this.props.match.params.artifact] || null;
-        const bentoServiceInfo = this.props.bentoServicesByArtifact[this.props.match.params.artifact] || null;
-        const loading = !(serviceInfo && bentoServiceInfo);
+        const artifact = this.props.match.params.artifact;
+        const serviceInfo = this.props.serviceInfoByArtifact[artifact] || null;
 
-        return loading ? <Skeleton /> : <>
-            <SitePageHeader title={serviceInfo.name}
-                        onBack={() => this.props.history.push(withBasePath("dashboard"))} />
-            <Layout>
-                <Layout.Content style={LAYOUT_CONTENT_STYLE}>
-                    <Typography.Title level={4}>Service Info</Typography.Title>
-                    <pre>{JSON.stringify(serviceInfo, null, 2)}</pre>
-                    <Typography.Title level={4}>Bento Service Configuration</Typography.Title>
-                    <pre>{JSON.stringify(bentoServiceInfo, null, 2)}</pre>
-                </Layout.Content>
-            </Layout>
+        const menuItems = pageMenu(artifact);
+        const selectedKeys = matchingMenuKeys(menuItems, urlPath(this.props.nodeInfo.CHORD_URL));
+
+        return <>
+            <SitePageHeader title={(serviceInfo || {}).name || ""}
+                            subTitle={(serviceInfo || {}).description || ""}
+                            footer={<Menu mode="horizontal" style={MENU_STYLE} selectedKeys={selectedKeys}>
+                                {menuItems.map(renderMenuItem)}
+                            </Menu>}
+                            onBack={() => this.props.history.push(withBasePath("dashboard"))} />
+            <Suspense fallback={<div style={{padding: "24px", backgroundColor: "white"}}><Skeleton active /></div>}>
+                <Switch>
+                    <Route exact path={withBasePath(`services/:artifact/overview`)}
+                           component={ServiceOverview} />
+                    <Route exact path={withBasePath(`services/:artifact/logs`)} component={ServiceLogs} />
+                    <Redirect from={withBasePath(`services/${artifact}`)}
+                              to={withBasePath(`services/${artifact}/overview`)} />
+                </Switch>
+            </Suspense>
         </>;
     }
 }
 
 ServiceDetail.propTypes = {
     serviceInfoByArtifact: PropTypes.objectOf(PropTypes.object),  // TODO
-    bentoServicesByArtifact: PropTypes.objectOf(PropTypes.object),  // TODO
+    nodeInfo: nodeInfoDataPropTypesShape,
 };
 
 const mapStateToProps = state => ({
     serviceInfoByArtifact: state.services.itemsByArtifact,
-    bentoServicesByArtifact: state.chordServices.itemsByArtifact,
+    nodeInfo: state.nodeInfo.data,
 });
 
 export default connect(mapStateToProps)(ServiceDetail);
---
+++ src/components/services/ServiceLogs.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class ServiceLogs extends Component {
+    render() {
+        return <div />;
+    }
+}
+
+export default ServiceLogs;
