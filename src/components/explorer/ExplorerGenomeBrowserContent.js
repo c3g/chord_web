d4c6743e6648d08da055e9ce0a576b572d3d98de
David Lougheed <david.lougheed@gmail.com>
2020-08-17 10:42:09 -0400

    Various improvements, WIP

--- src/components/explorer/ExplorerGenomeBrowserContent.js
+++ src/components/explorer/ExplorerGenomeBrowserContent.js
@@ -1,10 +1,11 @@
-import React, {Component} from "react";
+import React, {Component, createRef} from "react";
 
 import {Layout, Typography, Table, Divider} from "antd";
 import "antd/es/layout/style/css";
 
 import igv from "igv/js";
 import variants from '../../utils/features2.json';
+
 const ID_VARIANT = '71438220-d640-42f2-998d-e9c81adac710';
 
 import {LAYOUT_CONTENT_STYLE} from "../../styles/layoutContent";
@@ -13,94 +14,99 @@ class ExplorerGenomeBrowserContent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          igvContainer: null,
-          browser: null,
-          results: variants.results[ID_VARIANT],
-          locus: null,
-          columns: [],
-          search_id: ID_VARIANT,
-          matches: [],
-          calls: []
+            results: variants.results[ID_VARIANT],
+            locus: null,
+            search_id: ID_VARIANT,
+            matches: [],
+            calls: []
+        };
+
+        this.browser = null;
+        this.igvContainer = createRef();
+        this.igvOptions = this.igvOptions.bind(this);
+        this.configureBrowser = this.configureBrowser.bind(this);
+
+        // Column configuration for match table
+        this.columns = [
+            {
+                title: 'Chromosome',
+                dataIndex: 'chromosome',
+            },
+            {
+                title: 'Start',
+                dataIndex: 'start',
+            },
+            {
+                title: 'End',
+                dataIndex: 'end',
+            },
+            {
+                title: 'Reference Bases',
+                dataIndex: 'ref',
+            },
+            {
+                title: 'Alternate Bases',
+                dataIndex: 'alt',
+            },
+            {
+                title: 'Action',
+                render: (text, variant) => (<a onClick={() => this.changeLocus(variant)}>Show variant</a>)
+            },
+        ];
+    }
+
+    componentWillMount() {
+        // Configure the locus depending on the variants found
+        if (!this.state.results) {
+            this.setState({matches: [], calls: []});
+            return;
+        }
+
+        this.setState({
+            matches: this.state.results.matches,
+            calls: this.state.results.matches[0].calls,  // TODO: All calls
+        });
+        const first_match = this.state.results.matches[0];
+        this.setState({locus: `chr${first_match.chromosome}:${first_match.start}-${first_match.end}`});
+    }
+
+    igvOptions() {
+        return {
+            genome: "hg19",  // TODO: Based on dataset
+            locus: this.state.locus,
+            minimumBases: 40,
+            tracks: [
+                {
+                    type: "variant",
+                    sourceType: "ga4gh",
+                    format: 'vcf',
+                    variants: this.state.matches,
+                    variantSetId: this.state.search_id,
+                    calls: this.state.calls,
+                    name: "Variant Search",
+                    height: 100,
+                    squishedCallHeight: 1,
+                    expandedCallHeight: 10,
+                    displayMode: "expanded",
+                    visibilityWindow: 100,
+                },
+            ]
         };
     }
 
-    componentWillMount(){
-      // Configure the locus depending on the variants found
-      if(this.state.results){
-        this.setState({
-          matches: this.state.results.matches,
-          calls: this.state.results.matches[0].calls,
-        });
-        var first_match = this.state.results.matches[0];
-        this.setState({locus:`chr${first_match.chromosome}:${first_match.start}-${first_match.end}`});
-      }else{
-        this.setState({matches: [], calls: []});
-      }
+    configureBrowser(containerNode) {
+        igv.createBrowser(containerNode, this.igvOptions()).then(browser => this.browser = browser);
     }
 
     componentDidMount() {
-        this.state.igvContainer = document.getElementById("igv-container");
-
-        console.log(this.state.matches);
-        console.log(this.state.calls);
-
-        // Column configuration for match table
-        this.state.columns = [
-          {
-            title: 'Chromosome',
-            dataIndex: 'chromosome',
-          },
-          {
-            title: 'Start',
-            dataIndex: 'start',
-          },
-          {
-            title: 'End',
-            dataIndex: 'end',
-          },
-          {
-            title: 'Reference Base',
-            dataIndex: 'ref',
-          },
-          {
-            title: 'Alternate Base',
-            dataIndex: 'alt',
-          },
-          {
-             title: 'Action',
-             render: (text, variant) => ( <a onClick={() => this.changeLocus(variant)}> Show variant </a> )
-          }
-        ];
-
-        // Configure IGV browser options
-        const igvOptions = {
-            genome: "hg19",
-            locus: this.state.locus,
-            minimumBases:40,
-            tracks: [
-              {
-                type: "variant",
-                sourceType: "ga4gh",
-                format: 'vcf',
-                variants: this.state.matches,
-                variantSetId: this.state.search_id,
-                calls: this.state.calls,
-                name: "Variant Search",
-                height: 100,
-                squishedCallHeight: 1,
-                expandedCallHeight: 10,
-                displayMode: "expanded",
-                visibilityWindow: 100
-              }
-            ]
-        };
-        igv.createBrowser(this.state.igvContainer, igvOptions).then(browser => this.setState({browser: browser}));
+        this.configureBrowser(this.igvContainer.current
+            || document.getElementById("igv-container"));
     }
 
-    changeLocus(variant){
-      const newLocus = `chr${variant.chromosome}:${variant.start}-${variant.end}`;
-      this.setState({locus: newLocus});
-      this.state.browser.search(newLocus);
+    changeLocus(variant) {
+        const newLocus = `chr${variant.chromosome}:${variant.start}-${variant.end}`;
+        this.setState({locus: newLocus});
+        if (this.browser) this.browser.search(newLocus);
     }
 
     render() {
@@ -110,11 +116,11 @@ class ExplorerGenomeBrowserContent extends Component {
                 <div id="igv-container" />
                 <Divider orientation="left"> Match List </Divider>
                 <Table
-                  columns={this.state.columns}
-                  dataSource={this.state.matches}
-                  pagination={{ pageSize:10}}
-                  scroll={{ y:200 }}
-                 />
+                    columns={this.columns}
+                    dataSource={this.state.matches}
+                    pagination={{pageSize: 10}}
+                    scroll={{y: 200}}
+                />
             </Layout.Content>
         </Layout>;
     }
