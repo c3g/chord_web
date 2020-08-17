1bca65ec74ffa93d75abbd132458c098952e4deb
David Lougheed <david.lougheed@gmail.com>
2020-08-17 15:57:43 -0400

    Refact summary / initial age hist attempt

--- src/components/explorer/SearchSummaryModal.js
+++ src/components/explorer/SearchSummaryModal.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import {parse, toSeconds} from "iso8601-duration";
+
 import {Col, Divider, Modal, Row, Statistic, Typography} from "antd";
 import "antd/es/col/style/css";
 import "antd/es/divider/style/css";
@@ -7,7 +9,7 @@ import "antd/es/modal/style/css";
 import "antd/es/row/style/css";
 import "antd/es/statistic/style/css";
 
-import {VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryPie} from "victory";
+import {VictoryAxis, VictoryBar, VictoryChart, VictoryHistogram, VictoryLabel, VictoryPie} from "victory";
 import VictoryPieWrapSVG from "../VictoryPieWrapSVG";
 
 import {KARYOTYPIC_SEX_VALUES, SEX_VALUES} from "../../dataTypes/phenopacket";
@@ -25,6 +27,19 @@ const numObjectToVictoryArray = numObj => Object.entries(numObj)
     .map(([x, y]) => ({x, y}));
 
 
+// Bins of 10 years
+// TODO: Deal with start/end - for now, weight based on bin overlap?
+const AGE_HISTOGRAM_BINS = Array(10).keys().map(i => i * 10);
+
+
+const ageAndDOBToApproxYears = (age, dob=null) => {
+    const parsedAge = parse(age);
+    const parsedAgeSeconds = toSeconds(parsedAge, dob);
+    // Convert # of seconds to "normalized" years TODO: Sketchy logic
+    return parsedAgeSeconds / (60 * 60 * 24 * 365.2422);
+}
+
+
 const SearchSummaryModal = ({searchResults, ...props}) => {
     const searchFormattedResults = searchResults.searchFormattedResults || [];
 
@@ -34,33 +49,63 @@ const SearchSummaryModal = ({searchResults, ...props}) => {
 
     const numIndividualsBySex = Object.fromEntries(SEX_VALUES.map(v => [v, 0]));
     const numIndividualsByKaryotype = Object.fromEntries(KARYOTYPIC_SEX_VALUES.map(v => [v, 0]));
-    searchFormattedResults.forEach(r => {
-        numIndividualsBySex[r.individual.sex]++;
-        numIndividualsByKaryotype[r.individual.karyotypic_sex]++;
-    });
-
-    const individualsBySex = numObjectToVictoryArray(numIndividualsBySex);
-    const individualsByKaryotype = numObjectToVictoryArray(numIndividualsByKaryotype);
 
     // - Individuals' diseases summary - from phenopackets
-
     const numDiseasesByTerm = {};
-    searchFormattedResults.forEach(r => r.diseases.forEach(d => {
-        // TODO: Better ontology awareness - label vs id, translation, etc.
-        numDiseasesByTerm[d.term.label] = (numDiseasesByTerm[d.term.label] || 0) + 1;
-    }));
-    const diseasesByTerm = numObjectToVictoryArray(numDiseasesByTerm);
-    const maxDiseaseCount = Math.max(...diseasesByTerm.map(d => d.y));
 
     // Biosamples summary
     // TODO: More ontology aware
 
     const numSamplesByTissue = {};
-    searchFormattedResults.forEach(r => r.biosamples.forEach(b => {
-        const key = (b.sampled_tissue || {}).label || "N/A";
-        numSamplesByTissue[key] = (numSamplesByTissue[key] || 0) + 1;
-    }));
+    const numSamplesByTaxonomy = {};
+    const numSamplesByHistologicalDiagnosis = {};
+
+    const ageAtCollectionHistogram = [];
+
+    searchFormattedResults.forEach(r => {
+        if (r.individual) {
+            numIndividualsBySex[r.individual.sex]++;
+            numIndividualsByKaryotype[r.individual.karyotypic_sex]++;
+        }
+
+        (r.diseases || []).forEach(d => {
+            // TODO: Better ontology awareness - label vs id, translation, etc.
+            numDiseasesByTerm[d.term.label] = (numDiseasesByTerm[d.term.label] || 0) + 1;
+        });
+
+        (r.biosamples || []).forEach(b => {
+            const tissueKey = (b.sampled_tissue || {}).label || "N/A";
+            const taxonomyKey = (b.taxonomy || {}).label || "N/A";
+            const histDiagKey = (b.histological_diagnosis || {}).label || "N/A";
+            numSamplesByTissue[tissueKey] = (numSamplesByTissue[tissueKey] || 0) + 1;
+            numSamplesByTaxonomy[taxonomyKey] = (numSamplesByTaxonomy[taxonomyKey] || 0) + 1;
+            numSamplesByHistologicalDiagnosis[histDiagKey] = (numSamplesByHistologicalDiagnosis[histDiagKey] || 0) + 1;
+
+            if (b.individual_age_at_collection) {
+                let individualBirth = (r.individual || {}).date_of_birth;
+                if (individualBirth) {
+                    individualBirth = new Date(Date.parse(individualBirth));
+                }
+
+                const {age, start, end} = b.individual_age_at_collection;
+                if (age) {
+                    ageAtCollectionHistogram.push({x: ageAndDOBToApproxYears(age, individualBirth)});
+                } else if (start.age && end.age) {
+                    const startYears = ageAndDOBToApproxYears(start.age, individualBirth);
+                    const endYears = ageAndDOBToApproxYears(end.age, individualBirth);
+
+                    // Push average of years TODO: Better bin calculation?
+                    ageAtCollectionHistogram.push({x: (startYears + endYears) / 2});
+                }
+            }
+        })
+    });
+
+    const individualsBySex = numObjectToVictoryArray(numIndividualsBySex);
+    const individualsByKaryotype = numObjectToVictoryArray(numIndividualsByKaryotype);
+    const diseasesByTerm = numObjectToVictoryArray(numDiseasesByTerm);
     const samplesByTissue = numObjectToVictoryArray(numSamplesByTissue);
+    const samplesByTaxonomy = numObjectToVictoryArray(numSamplesByTaxonomy);
 
     // TODO: Procedures: account for both code and (if specified) body_site
     //  e.g. "Biopsy" or "Biopsy on X body site" - maybe stacked bar or grouped bar chart?
@@ -73,7 +118,7 @@ const SearchSummaryModal = ({searchResults, ...props}) => {
             <Col span={12}>
                 <Statistic title="Biosamples"
                            value={searchFormattedResults
-                               .map(i => i.biosamples.length)
+                               .map(i => (i.biosamples || []).length)
                                .reduce((s, v) => s + v, 0)} />
             </Col>
         </Row>
@@ -95,7 +140,7 @@ const SearchSummaryModal = ({searchResults, ...props}) => {
                     </VictoryPieWrapSVG>
                 </Col>
                 <Col span={12}>
-                    <VictoryChart domainPadding={32}>
+                    <VictoryChart>
                         <VictoryAxis />
                         <VictoryBar data={diseasesByTerm} {...VICTORY_LABEL_PROPS} />
                         <VictoryLabel text="DISEASE" {...VICTORY_BAR_TITLE_PROPS} />
@@ -113,6 +158,23 @@ const SearchSummaryModal = ({searchResults, ...props}) => {
                         <VictoryPie data={samplesByTissue} {...VICTORY_PIE_PROPS} />
                         <VictoryLabel text="TISSUE" {...VICTORY_PIE_LABEL_PROPS} />
                     </VictoryPieWrapSVG>
+                </Col>
+                <Col span={12}>
+                    <VictoryPieWrapSVG>
+                        <VictoryPie data={samplesByTaxonomy} {...VICTORY_PIE_PROPS} />
+                        <VictoryLabel text="TAXONOMY" {...VICTORY_PIE_LABEL_PROPS} />
+                    </VictoryPieWrapSVG>
+                </Col>
+            </Row>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <VictoryChart>
+                        <VictoryHistogram data={ageAtCollectionHistogram}
+                                          bins={AGE_HISTOGRAM_BINS} />
+                    </VictoryChart>
+                </Col>
+                <Col span={12}>
+                    {/* TODO: histological_diagnosis pie chart */}
                 </Col>
             </Row>
         </> : null}
