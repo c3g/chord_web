0ae4842793240fc53c923342d5cb47df1ed84be7
David Lougheed <david.lougheed@gmail.com>
2020-10-30 19:37:45 -0400

    Make federation mode default to true

--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,10 @@
 const ENV = (process || {}).env || {};
 const SETTINGS_FALSE_VALUES = ["", "false"];
 
-// TODO: System for truthy defaults
-
-const isFalsey = ev => !SETTINGS_FALSE_VALUES.includes((ev || "").toString().toLocaleLowerCase().trim());
+const isFalsey = (ev, df) => !SETTINGS_FALSE_VALUES.includes(
+    (ev === undefined ? df : ev).toString().toLocaleLowerCase().trim());
 
 // Setting toggle to control whether the discovery/peering aspects are present
 // in the deployment of the front end. False-y values include an empty string,
 // "false", or "0".
-export const FEDERATION_MODE = isFalsey(ENV.BENTO_FEDERATION_MODE);
+export const FEDERATION_MODE = isFalsey(ENV.BENTO_FEDERATION_MODE, true);
