6d34f4d8d7b761cb6f5294bc66152454e6d0a39e
David Lougheed <david.lougheed@gmail.com>
2020-11-09 21:19:13 -0500

    Fix env var for federation mode

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,3 @@
-const ENV = (process || {}).env || {};
 const SETTINGS_FALSE_VALUES = ["", "false"];
 
 const isFalsey = (ev, df) => !SETTINGS_FALSE_VALUES.includes(
@@ -7,6 +6,4 @@ const isFalsey = (ev, df) => !SETTINGS_FALSE_VALUES.includes(
 // Setting toggle to control whether the discovery/peering aspects are present
 // in the deployment of the front end. False-y values include an empty string,
 // "false", or "0".
-export const FEDERATION_MODE = isFalsey(ENV.BENTO_FEDERATION_MODE, true);
-
-console.log(FEDERATION_MODE, ENV, process.env.CHORD_URL, process.env.BENTO_FEDERATION_MODE, process.env);
+export const FEDERATION_MODE = isFalsey(process.env.BENTO_FEDERATION_MODE, true);
