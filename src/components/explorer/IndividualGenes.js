0c2ee717982dbdc2c7c08badca72019cdb5c2b70
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2020-12-16 11:17:03 -0500

    linting

--- src/components/explorer/IndividualGenes.js
+++ src/components/explorer/IndividualGenes.js
@@ -3,11 +3,8 @@ import React from "react";
 import {Table} from "antd";
 import "antd/es/table/style/css";
 
-import {EM_DASH} from "../../constants";
-import {renderOntologyTerm} from "./ontologies";
 import {individualPropTypesShape} from "../../propTypes";
 
-import { PropTypes }from "prop-types";
 // TODO: Only show genes from the relevant dataset, if specified;
 //  highlight those found in search results, if specified
 
@@ -16,7 +13,7 @@ const IndividualGenes = ({individual}) =>
     const biosamples = (individual || {}).phenopackets.flatMap(p => p.biosamples);
     const genes = (individual || {}).phenopackets.flatMap(p => p.genes);
     const genesFlat = genes.flatMap(g => g.symbol);
-    const ids = (biosamples || []).map(b => 
+    const ids = (biosamples || []).map(_b => 
         ({
             //title: `Genes`,
             // key: "id",
@@ -24,16 +21,16 @@ const IndividualGenes = ({individual}) =>
             //sorter: (a, b) => a.id.localeCompare(b.id),
             //defaultSortOrder: "ascend"
         })
-    )
+    );
 
     return <Table bordered
-           size="middle"
-           pagination={{pageSize: 25}}
-           columns={ids}
-           rowKey="id"
-           dataSource={genesFlat} 
-        />
-    };
+                  size="middle"
+                  pagination={{pageSize: 25}}
+                  columns={ids}
+                  rowKey="id"
+                  dataSource={genesFlat} 
+        />;
+};
 IndividualGenes.propTypes = {
     individual: individualPropTypesShape
     //PropTypes.array,
--- src/components/explorer/IndividualVariants.js
+++ src/components/explorer/IndividualVariants.js
@@ -3,48 +3,10 @@ import React from "react";
 import {Table} from "antd";
 import "antd/es/table/style/css";
 
-import {EM_DASH} from "../../constants";
-import {renderOntologyTerm} from "./ontologies";
 import {individualPropTypesShape} from "../../propTypes";
 
-import { PropTypes }from "prop-types";
 // TODO: Only show variants from the relevant dataset, if specified;
 //  highlight those found in search results, if specified
-
-// const VARIANTS_COLUMNS = [
-//     {
-//         title: "ID",
-//         key: "id",
-//         render: (_, individual) => individual.id,
-//         sorter: (a, b) => a.id.localeCompare(b.id),
-//         defaultSortOrder: "ascend",
-//     },
-//     {
-//         title: "Sampled Tissue",
-//         key: "sampled_tissue",
-//         render: (_, individual) => renderOntologyTerm(individual.sampled_tissue),
-//     },
-//     {
-//         title: "Procedure",
-//         key: "procedure",
-//         render: (_, individual) => <>
-//             <strong>Code:</strong> {renderOntologyTerm(individual.procedure.code)}<br />
-//             {individual.procedure.body_site
-//                 ? <><br /><strong>Body Site:</strong> {renderOntologyTerm(individual.procedure.body_site)}</>
-//                 : null}
-//         </>,
-//     },
-//     {
-//         title: "Ind. Age at Collection",
-//         key: "individual_age_at_collection",
-//         render: (_, individual) => {
-//             const age = individual.individual_age_at_collection;
-//             return age
-//                 ? (age.hasOwnProperty("age") ? age.age : `Between ${age.start.age} and ${age.end.age}`)
-//                 : EM_DASH;
-//         },
-//     },
-// ];
 
 const IndividualVariants = ({individual}) =>
 {
@@ -58,16 +20,16 @@ const IndividualVariants = ({individual}) =>
             //sorter: (a, b) => a.id.localeCompare(b.id),
             //defaultSortOrder: "ascend"
         })
-    )
+    );
 
     return <Table bordered
-           size="middle"
-           pagination={{pageSize: 25}}
-           columns={ids}
-           rowKey="id"
-           dataSource={variantsFlat} 
-        />
-    };
+                  size="middle"
+                  pagination={{pageSize: 25}}
+                  columns={ids}
+                  rowKey="id"
+                  dataSource={variantsFlat} 
+        />;
+};
         
 
 IndividualVariants.propTypes = {
